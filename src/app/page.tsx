6beed0950c74fa1e3049a8f92415a304e898fcc9
Trey Jackson <jacksrf@gmail.com>
2025-05-07 13:28:29 -0500

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -155,6 +155,7 @@ export default function Home() {
           pin: container,
           start: "top top=+80px",
           end: "bottom bottom",
+
           scrub: 1,
           onUpdate: (self) => {
             const closestIndex = Math.round(self.progress * (numProjects - 1));
@@ -207,61 +208,61 @@ export default function Home() {
     animateToProject(projectId, clickedElement);
   };
 
-  // if (!selectedVersion) {
-  //   return (
-  //     <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-  //       <div className="max-w-7xl mx-auto">
-  //         <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4">
-  //           <div className="flex">
-  //             <div className="flex-shrink-0">
-  //               <svg className="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
-  //                 <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-  //               </svg>
-  //             </div>
-  //             <div className="ml-3">
-  //               <p className="text-sm text-yellow-700">
-  //                 Please select a Bible version to view the books.
-  //               </p>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  if (!selectedVersion) {
+    return (
+      <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto">
+          <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                {/* <svg className="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg> */}
+              </div>
+              <div className="ml-3">
+                {/* <p className="text-sm text-yellow-700">
+                  Please select a Bible version to view the books.
+                </p> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-  // if (loading) {
-  //   return (
-  //     <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-  //       <div className="max-w-7xl mx-auto">
-  //         <div className="flex items-center justify-center">
-  //           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex items-center justify-center">
+            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-  // if (error) {
-  //   return (
-  //     <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-  //       <div className="max-w-7xl mx-auto">
-  //         <div className="bg-red-50 border-l-4 border-red-400 p-4">
-  //           <div className="flex">
-  //             <div className="flex-shrink-0">
-  //               <svg className="h-5 w-5 text-red-400" viewBox="0 0 20 20" fill="currentColor">
-  //                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
-  //               </svg>
-  //             </div>
-  //             <div className="ml-3">
-  //               <p className="text-sm text-red-700">{error}</p>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  if (error) {
+    return (
+      <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto">
+          <div className="bg-red-50 border-l-4 border-red-400 p-4">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                {/* <svg className="h-5 w-5 text-red-400" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                </svg> */}
+              </div>
+              <div className="ml-3">
+                {/* <p className="text-sm text-red-700">{error}</p> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div ref={containerRef} className="mwg_effect038">
@@ -269,9 +270,9 @@ export default function Home() {
         <div className="books_container">
           {books.map((book) => (
             <div
-              key={book.id}
+              key={book.name}
               className="project"
-              onClick={(e) => handleProjectClick(e, book.id)}
+              onClick={(e) => handleProjectClick(e, book.name)}
             >
               <div className="datas">
                 <h1 className="label">{book.name}</h1>
