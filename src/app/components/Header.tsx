3f7810fc5f5ed9d5270bf7fec64edb412da219b6
Trey Jackson <jacksrf@gmail.com>
2025-05-08 11:07:40 -0500

    updates

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
         <div className="mr-4 flex">
-          <Link href="/" className="mr-6 flex items-center space-x-2">
+          <Link href="/books" className="mr-6 flex items-center space-x-2">
             <span className="font-bold">The Lumen Bible</span>
           </Link>
         </div>
